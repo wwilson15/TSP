67e8048ca5d84b39483632fe88d30a5ea5d25554
Wade Wilson <wadewilson@Wades-MacBook-Pro.local>
2017-03-15 10:46:58 -0700

    Initial Commit

---
+++ Projct Gamma/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projct Gamma
+//
+//  Created by Wade Wilson on 3/15/17.
+//  Copyright © 2017 Wade Wilson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
